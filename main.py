cf7f1ae751b52875a4674201bfcd965e9f56fc4c
mrhirob-droid <mrhirob@icloud.com>
2026-05-21 13:33:00 -0700

    Apple

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import snake
+import Apple
 import ai
 
 def main():
+    res = 15
     print("start")
-    player_snake = snake.Snake()
+    player_snake = snake.Snake(res)
+    apple = Apple.Apple(res)
 
     try:
         while True:
             # main loop
+            #eating
+            if(player_snake.snake_head == apple.applePos):
+                player_snake.grow
+                apple.generate(player_snake)
             pass
     except KeyboardInterrupt:
         print("Program stopped by user.")
